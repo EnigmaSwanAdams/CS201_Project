5581f0ba90c8ee127fdfe63352e01889f8ceb28d
Enigma Swan Adams <eswanadams@alaska.edu>
2021-04-26 00:15:37 -0800

    simplified, and added option to multiply two images together

--- multiply_main.cpp
+++ multiply_main.cpp
@@ -20,38 +20,49 @@ using std::istringstream;
 int main() {
 	string line;
 	string inFile;
-	string hexCode;
+	string mLayer;
 	string layerType;
 
 	//letting people know what the program does
 	cout << "This program takes an image (that must be in the same file as the program and in .ppm format) \n"
 		"and writes a new file with an added multiply layer. " << endl;
+	cout << "You can input one image and one hexidecimal color code to apply a solid color multiply layer. \n"
+		"Or you can input 2 .ppm files and the second one will act as the multiply layer." << endl; // actually it doesn't matter which one 
+		//is on top here because the multiply blend mode is commutative
 
 	
 	while (true) {
 		//get strings for the different pieces of info needed
 		cout << "please enter the file name "; cin >> inFile;
 		//cout << "Please enter the layer type "; cin >> layerType; 
-		cout << "Please enter the hexidecimal color for the layer that will be added "; cin >> hexCode; cout << endl;
-		
-		//get hex number for rgb
-		string strr = hexCode.substr(0, 2);
-		string strg = hexCode.substr(2, 2);
-		string strb = hexCode.substr(4, 2);
-		//convert the string hex rgb numbers to strings
-		int r = stoi(strr, 0, 16);
-		int g = stoi(strg, 0, 16);
-		int b = stoi(strb, 0, 16);
-
-		// read int he file and get info using the class RGBimage
-		RGBImage image(inFile);
+		cout << "Please enter a hexidecimal color, or another .ppm image file name "; cin >> mLayer; cout << endl;
 		
 
-		//decide which member function to run, and run it
-		//if (layerType == "multiply") {
-			//cout << "you chose a mupltiply layer";
-			image.writeMultipliedPPMfile(inFile, r, g, b);
-		//}
+		if (mLayer.substr(mLayer.size() - 4) == ".ppm") {
+			RGBImage image1(inFile);
+			RGBImage image2(mLayer);
+			image1.writeMultipliedPPMfile(image1, image2);
+		}
+
+		
+		else {
+			//get hex number for rgb
+			string strr = mLayer.substr(0, 2);
+			string strg = mLayer.substr(2, 2);
+			string strb = mLayer.substr(4, 2);
+			//convert the string hex rgb numbers to strings
+			int r = stoi(strr, 0, 16);
+			int g = stoi(strg, 0, 16);
+			int b = stoi(strb, 0, 16);
+
+
+			// read int he file and get info using the class RGBimage
+			RGBImage image(inFile);
+
+			//use member function to write the new image
+			image.writeMultipliedPPMfileSolid(inFile, r, g, b);
+			
+		}
 	}
 	
 	return 0;
